6733358ae6f8e78844e8f1dff3346797750314d8
Ayush <ayushsharma1198@gmail.com>
2020-09-25 21:29:57 +0530

    q1 c done

--- q1/triwizard.py
+++ q1/triwizard.py
@@ -79,8 +79,30 @@ def incendio(my_integer):
 # 'm': integer that divides n
 # return type: int numpy Ndarray; dim: nxn
 def alohomora(n, m):
-	
-	pass
+	pattern1=np.ones((m,m),dtype=int)
+	pattern2=np.zeros((m,m),dtype=int)
+	result=None
+	for row in range(0,int(n/m)):
+		temp=None
+		if row%2==0:
+			temp=np.copy(pattern1)
+			for col in range(1,int(n/m)):
+				if col%2==0:
+					temp=np.hstack((temp,pattern1))
+				else:
+					temp=np.hstack((temp,pattern2))
+		else:
+			temp=np.copy(pattern2)
+			for col in range(1,int(n/m)):
+				if col%2==1:
+					temp=np.hstack((temp,pattern1))
+				else:
+					temp=np.hstack((temp,pattern2))
+		if row==0:
+			result=np.copy(temp)
+		else:
+			result=np.vstack((result,temp))
+	return result
 
 
 # ---- Task (d) -------
