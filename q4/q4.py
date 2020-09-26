272d76d5cdec19f795b261b87a70f2aade50cd74
Ayush <ayushsharma1198@gmail.com>
2020-09-26 20:54:17 +0530

    readme updated

--- q4/q4.py
+++ q4/q4.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+from stackapi import StackAPI
+from functools import reduce
+if __name__ == "__main__":
+    words=input().split(sep=' ')
+    #tags=reduce(lambda x,y: x+' '+y,words)
+    tags='python and numpy'
+    SITE=StackAPI('stackoverflow')
+    questions=SITE.fetch('questions',tagged=tags,max=20,sort='votes')
+    #print(questions)
+    for ele in questions['items']:
+        print(ele['tags'])
--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
-Ayush Sharma (203050029 / ayushsharma) (Github: ayush-qubit) -
+Ayush Sharma (203050029 / ayushsharma) (Github: ayush-qubit) - q1(a,c,d,e,g)
 Vikrant Sharma (203050035 / vikrantsharma) (Github: Vikrant7981) - q1(b),q1(f),q2,q3
 Utkarsh Indolia (203050111 / utkarshindolia) (Github: utkarsh92) - q6
 Bharat Panwar (203050113 / nishkam) (GitHub: nishhhkam) -
