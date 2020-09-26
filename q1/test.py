af62193e7bf48de42e45184696c4ee81d7cb8948
Ayush <ayushsharma1198@gmail.com>
2020-09-26 14:02:41 +0530

    q1 (f) not done

--- q1/test.py
+++ q1/test.py
@@ -1,5 +1,5 @@
 import numpy as np
 
 k=2
-arr=np.array([[6,4,4,7],[7,8,1,0],[11,10,5,11]])
-print(np.argsort(-1*arr,axis=1,kind='heap')[:,0:k])
+arr=np.array([[6,4,4,7],[7,8,1,0],[11,11,10,5]])
+print(np.flip(np.argsort(arr,axis=1,kind='stable'),axis=1)[:,0:k])
--- q1/triwizard.py
+++ q1/triwizard.py
@@ -147,4 +147,4 @@ def leviosa(arr, k):
 # k: integer
 # return type: n-D integer array; dim: (m,k)
 def accio(mat, k):
-	return np.argsort(-1*mat,axis=1,kind='heap')[:,0:k]
+	return np.flip(np.argsort(mat,axis=1,kind='stable'),axis=1)[:,0:k]
