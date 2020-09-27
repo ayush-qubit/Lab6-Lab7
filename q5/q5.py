68cb4c1050a87d0c4e7dbcaa2323759118c78323
Ayush <ayushsharma1198@gmail.com>
2020-09-27 15:48:31 +0530

    q5 updated

--- q5/q5.py
+++ q5/q5.py
@@ -8,4 +8,16 @@ def get_data(csv_path):
     instance_3=data_grouped.get_group('instance-3')
     return instance_1,instance_2,instance_3
 
+def graph_instance_1(instance_1):
+    instance_1_grouped=instance_1.groupby('algorithm')
+    a1=instance_1_grouped.get_group('round-robin')
+    a2=instance_1_grouped.get_group('ucb')
+    a3=instance_1_grouped.get_group('kl-ucb')
+    a4=instance_1_grouped.get_group('thompson-sampling')
+    a5,a6,a7=split_epsilon_greedy(instance_1_grouped.get_group('epsilon-greedy'))
+
+def split_epsilon_greedy(a5):
+    a5_grouped=a5.groupby('epsilon')
+    return a5_grouped.get_group('0'),a5_grouped.get_group('0.2'),a5_grouped.get_group('0.02')
+
 i1,i2,i3=get_data('/home/ayush/git-workspace/Lab6-Lab7/q5/data.csv')
