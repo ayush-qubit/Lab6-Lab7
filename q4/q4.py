7579bc803feddbc846e835998f5c08b8d345f3f5
Ayush <ayushsharma1198@gmail.com>
2020-09-27 12:26:07 +0530

    initial commit

--- q4/q4.py
+++ q4/q4.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 from stackapi import StackAPI
-from functools import reduce
+from bs4 import BeautifulSoup
+import requests
+
+def check_accepted_answer(d):
+    
+    pass
+
 def save_to_csv(csv_path):
     pass
+
 if __name__ == "__main__":
     tags=list(map(lambda x: x.lower(),input().split(sep=' ')))
     print(tags)
@@ -10,5 +17,4 @@ if __name__ == "__main__":
     questions=SITE.fetch('questions',tagged=tags,max=50,sort='votes')
     for ele in questions['items']:
         if all(x in ele['tags'] for x in tags):
-            for ele2 in ele['owner']:
-                print(type(ele2))
+            pass
