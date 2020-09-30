1a7875a1898289ce4c21604d837e045425ca092d
utkarsh92 <utkarshindolia@cse.iitb.ac.in>
2020-10-01 03:50:02 +0530

    q4 done

--- q4/q4.py
+++ q4/q4.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python3
 from stackapi import StackAPI
-from bs4 import BeautifulSoup
-import requests
+import numpy as np
+#import pprint
+#import sys
 
-def check_accepted_answer(question_id,SITE):
-    answers=SITE.fetch('questions/' + str(question_id) + '/answers', order = 'desc', sort='votes')
-    print(answers['items'][0]['answer_id'])
-    pass
+tags = list(map(str.lower, input().split()))
+#print(tags)
+#tags = ['python','numpy']
+fname = '_'.join(map(str,tags))
 
-def save_to_csv(csv_path):
-    pass
+SITE = StackAPI('stackoverflow', key='eV18T0ejHKZNnSWR6YDmdQ((')
+SITE.page_size = 50
+SITE.max_pages = 1
+ques = SITE.fetch('questions', tagged=';'.join(map(str,tags)), sort='votes')
+#pp = pprint.PrettyPrinter(indent=4)
+#pp.pprint(ques)
+#print('quota_remaining:', ques['quota_remaining'])
+final = []
+for item in ques['items']:
+    
+    if 'accepted_answer_id' in item:
+        
+        qid = item['question_id']
+        utag = fname
+        link = item['link']
+        qtag = '"' + str(item['tags']) + '"'
+        ans = link + "/#" + str(item['accepted_answer_id'])
 
-if __name__ == "__main__":
-    tags=list(map(lambda x: x.lower(),input().split(sep=' ')))
-    print(tags)
-    SITE=StackAPI('stackoverflow')
-    questions=SITE.fetch('questions',tagged=tags,max=50,sort='votes')
-    for ele in questions['items']:
-        if all(x in ele['tags'] for x in tags):
-            check_accepted_answer(ele['question_id'],SITE)
-            input()
-            pass
+        final.append([qid, utag, link, qtag, ans])
+
+final = np.array(final)
+
+np.savetxt(
+    fname + '.csv',
+    final,
+    fmt='%s',
+    delimiter=',', 
+    header="question_id,tag,link,tags,accepted_answer",
+    comments="")
