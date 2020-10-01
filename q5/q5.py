ee6255d8a397b667527556e1b4874a96d3d97073
utkarsh92 <utkarshindolia@cse.iitb.ac.in>
2020-10-01 20:38:00 +0530

    q5 folder update

--- q5/q5.py
+++ q5/q5.py
@@ -1,111 +1,231 @@
-import pandas as pd
-import random
 import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import argparse
+import sys
 
+plt.rcParams['figure.figsize'] = (10.0, 5.0)
 
-def get_data(csv_path):
-    data=pd.read_csv(csv_path)
-    data_grouped=data.groupby('instance')
-    instance_1=data_grouped.get_group('instance-1')
-    instance_2=data_grouped.get_group('instance-2')
-    instance_3=data_grouped.get_group('instance-3')
-    return instance_1,instance_2,instance_3
-
-def graph_instance_1(instance_1):
-    instance_1_grouped=instance_1.groupby('algorithm')
-    a1=instance_1_grouped.get_group('round-robin')
-    a2=instance_1_grouped.get_group('ucb')
-    a3=instance_1_grouped.get_group('kl-ucb')
-    a4=instance_1_grouped.get_group('thompson-sampling')
-    a5,a6,a7=split_epsilon_greedy(instance_1_grouped.get_group('epsilon-greedy'))
-    algos=[a1,a2,a3,a4,a5,a6,a7]
-    values=[]
-    colors=['red','blue','green','pink','gray','violet','yellow']
-    i=0
-    plt.clf()
-    for algo in algos:
-        #values.append((algo,get_random_horizon(algo)))
-        points=get_random_horizon(algo)
-        plt.plot(points[:,0],points[:,1],color=colors[i])
-        i=i+1
-    plt.xlabel('Horizon')
-    plt.ylabel('Regret')
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.savefig('./instance1.png')
-
-def split_epsilon_greedy(a5):
-    a5_grouped=a5.groupby('epsilon')
-    return a5_grouped.get_group(float('0.2')),a5_grouped.get_group(float('0.02')),a5_grouped.get_group(float('0.002'))
-
-def get_random_horizon(algo):
-    algo_grouped=algo.groupby('horizon')
-    horizon=[]
-    for name,group in algo_grouped:
-        regret=np.array(pd.to_numeric(group['regret']))
-        regret=np.random.choice(regret,50)
-        average_value=regret.sum()/50.0
-        l=[]
-        l.append(int(name))
-        l.append(average_value)
-        horizon.append(l)
-    return np.array(horizon)
-    pass
-
-def graph_instance_2(instance_2):
-    instance_2_grouped=instance_2.groupby('algorithm')
-    a1=instance_2_grouped.get_group('round-robin')
-    a2=instance_2_grouped.get_group('ucb')
-    a3=instance_2_grouped.get_group('kl-ucb')
-    a4=instance_2_grouped.get_group('thompson-sampling')
-    a5,a6,a7=split_epsilon_greedy(instance_2_grouped.get_group('epsilon-greedy'))
-    algos=[a1,a2,a3,a4,a5,a6,a7]
-    values=[]
-    colors=['red','blue','green','pink','gray','violet','yellow']
-    i=0
-    plt.clf()
-    for algo in algos:
-        #values.append((algo,get_random_horizon(algo)))
-        points=get_random_horizon(algo)
-        plt.plot(points[:,0],points[:,1],color=colors[i])
-        i=i+1
-    plt.xlabel('Horizon')
-    plt.ylabel('Regret')
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.savefig('./instance2.png')
-
-def graph_instance_3(instance_3):
-    instance_3_grouped=instance_3.groupby('algorithm')
-    a1=instance_3_grouped.get_group('round-robin')
-    a2=instance_3_grouped.get_group('ucb')
-    a3=instance_3_grouped.get_group('kl-ucb')
-    a4=instance_3_grouped.get_group('thompson-sampling')
-    a5,a6,a7=split_epsilon_greedy(instance_3_grouped.get_group('epsilon-greedy'))
-    algos=[a1,a2,a3,a4,a5,a6,a7]
-    values=[]
-    colors=['red','blue','green','pink','gray','violet','yellow']
-    i=0
-    plt.clf()
-    for algo in algos:
-        #values.append((algo,get_random_horizon(algo)))
-        points=get_random_horizon(algo)
-        plt.plot(points[:,0],points[:,1],color=colors[i])
-        i=i+1
-    plt.xlabel('Horizon')
-    plt.ylabel('Regret')
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.savefig('./instance3.png')
-
-
+# Reading Data
 parser=argparse.ArgumentParser()
 parser.add_argument('--data',required=True,help='Data file')
 args=vars(parser.parse_args())
 csv_path=args['data']
-i1,i2,i3=get_data(csv_path)
-graph_instance_1(i1)
-graph_instance_2(i2)
-graph_instance_3(i3)
+data = pd.read_csv(csv_path)
+
+
+#Instance1
+x1 = []
+y1= []
+grouped = data.groupby(['instance','algorithm','horizon'])
+regavg = grouped['regret'].agg(np.mean)
+regavgrb = regavg[('instance-1','round-robin')]
+for i, v in regavgrb.items():
+    x1.append(i)
+    y1.append(v)
+x2 = []
+y2= []
+regavgrb = regavg[('instance-1','ucb')]
+for i, v in regavgrb.items():
+    x2.append(i)
+    y2.append(v)
+x3 = []
+y3= []
+regavgrb = regavg[('instance-1','kl-ucb')]
+for i, v in regavgrb.items():
+    x3.append(i)
+    y3.append(v)
+x4 = []
+y4= []
+regavgrb = regavg[('instance-1','thompson-sampling')]
+for i, v in regavgrb.items():
+    x4.append(i)
+    y4.append(v)
+
+
+x5 = []
+y5= []
+grouped = data.groupby(['instance','algorithm','epsilon','horizon'])
+regavg = grouped['regret'].agg(np.mean)
+regavgrb = regavg[('instance-1','epsilon-greedy',0.2)]
+for i, v in regavgrb.items():
+    x5.append(i)
+    y5.append(v)
+
+x6 = []
+y6= []
+grouped = data.groupby(['instance','algorithm','epsilon','horizon'])
+regavg = grouped['regret'].agg(np.mean)
+regavgrb = regavg[('instance-1','epsilon-greedy',0.02)]
+for i, v in regavgrb.items():
+    x6.append(i)
+    y6.append(v)
+
+x7 = []
+y7= []
+grouped = data.groupby(['instance','algorithm','epsilon','horizon'])
+regavg = grouped['regret'].agg(np.mean)
+regavgrb = regavg[('instance-1','epsilon-greedy',0.002)]
+for i, v in regavgrb.items():
+    x7.append(i)
+    y7.append(v)
+
+plt.plot(x1, y1, label = "round-robin")
+plt.plot(x2, y2, label = "ucb")
+plt.plot(x3, y3, label = "kl-ucb")
+plt.plot(x4, y4, label = "thompson-sampling")
+plt.plot(x5, y5, label = "epsilon-greedy-0.2")
+plt.plot(x6, y6, label = "epsilon-greedy-0.02")
+plt.plot(x7, y7, label = "epsilon-greedy-0.002")
+plt.xlabel('horizon')
+plt.ylabel('regret')
+plt.title('instance-1')
+plt.xscale('log')
+plt.yscale('log')
+plt.legend()
+plt.show()
+
+
+#Instance2
+x1 = []
+y1= []
+grouped = data.groupby(['instance','algorithm','horizon'])
+regavg = grouped['regret'].agg(np.mean)
+regavgrb = regavg[('instance-2','round-robin')]
+for i, v in regavgrb.items():
+    x1.append(i)
+    y1.append(v)
+x2 = []
+y2= []
+regavgrb = regavg[('instance-2','ucb')]
+for i, v in regavgrb.items():
+    x2.append(i)
+    y2.append(v)
+x3 = []
+y3= []
+regavgrb = regavg[('instance-2','kl-ucb')]
+for i, v in regavgrb.items():
+    x3.append(i)
+    y3.append(v)
+x4 = []
+y4= []
+regavgrb = regavg[('instance-2','thompson-sampling')]
+for i, v in regavgrb.items():
+    x4.append(i)
+    y4.append(v)
+
+
+x5 = []
+y5= []
+grouped = data.groupby(['instance','algorithm','epsilon','horizon'])
+regavg = grouped['regret'].agg(np.mean)
+regavgrb = regavg[('instance-2','epsilon-greedy',0.2)]
+for i, v in regavgrb.items():
+    x5.append(i)
+    y5.append(v)
+
+x6 = []
+y6= []
+grouped = data.groupby(['instance','algorithm','epsilon','horizon'])
+regavg = grouped['regret'].agg(np.mean)
+regavgrb = regavg[('instance-2','epsilon-greedy',0.02)]
+for i, v in regavgrb.items():
+    x6.append(i)
+    y6.append(v)
+
+x7 = []
+y7= []
+grouped = data.groupby(['instance','algorithm','epsilon','horizon'])
+regavg = grouped['regret'].agg(np.mean)
+regavgrb = regavg[('instance-2','epsilon-greedy',0.002)]
+for i, v in regavgrb.items():
+    x7.append(i)
+    y7.append(v)
+
+plt.plot(x1, y1, label = "round-robin")
+plt.plot(x2, y2, label = "ucb")
+plt.plot(x3, y3, label = "kl-ucb")
+plt.plot(x4, y4, label = "thompson-sampling")
+plt.plot(x5, y5, label = "epsilon-greedy-0.2")
+plt.plot(x6, y6, label = "epsilon-greedy-0.02")
+plt.plot(x7, y7, label = "epsilon-greedy-0.002")
+plt.xlabel('horizon')
+plt.ylabel('regret')
+plt.title('instance-2')
+plt.xscale('log')
+plt.yscale('log')
+plt.legend()
+plt.show()
+
+#Instance3
+x1 = []
+y1= []
+grouped = data.groupby(['instance','algorithm','horizon'])
+regavg = grouped['regret'].agg(np.mean)
+regavgrb = regavg[('instance-3','round-robin')]
+for i, v in regavgrb.items():
+    x1.append(i)
+    y1.append(v)
+x2 = []
+y2= []
+regavgrb = regavg[('instance-3','ucb')]
+for i, v in regavgrb.items():
+    x2.append(i)
+    y2.append(v)
+x3 = []
+y3= []
+regavgrb = regavg[('instance-3','kl-ucb')]
+for i, v in regavgrb.items():
+    x3.append(i)
+    y3.append(v)
+x4 = []
+y4= []
+regavgrb = regavg[('instance-3','thompson-sampling')]
+for i, v in regavgrb.items():
+    x4.append(i)
+    y4.append(v)
+
+
+x5 = []
+y5= []
+grouped = data.groupby(['instance','algorithm','epsilon','horizon'])
+regavg = grouped['regret'].agg(np.mean)
+regavgrb = regavg[('instance-3','epsilon-greedy',0.2)]
+for i, v in regavgrb.items():
+    x5.append(i)
+    y5.append(v)
+
+x6 = []
+y6= []
+grouped = data.groupby(['instance','algorithm','epsilon','horizon'])
+regavg = grouped['regret'].agg(np.mean)
+regavgrb = regavg[('instance-3','epsilon-greedy',0.02)]
+for i, v in regavgrb.items():
+    x6.append(i)
+    y6.append(v)
+
+x7 = []
+y7= []
+grouped = data.groupby(['instance','algorithm','epsilon','horizon'])
+regavg = grouped['regret'].agg(np.mean)
+regavgrb = regavg[('instance-3','epsilon-greedy',0.002)]
+for i, v in regavgrb.items():
+    x7.append(i)
+    y7.append(v)
+
+plt.plot(x1, y1, label = "round-robin")
+plt.plot(x2, y2, label = "ucb")
+plt.plot(x3, y3, label = "kl-ucb")
+plt.plot(x4, y4, label = "thompson-sampling")
+plt.plot(x5, y5, label = "epsilon-greedy-0.2")
+plt.plot(x6, y6, label = "epsilon-greedy-0.02")
+plt.plot(x7, y7, label = "epsilon-greedy-0.002")
+plt.xlabel('horizon')
+plt.ylabel('regret')
+plt.title('instance-3')
+plt.xscale('log')
+plt.yscale('log')
+plt.legend()
+plt.show()
