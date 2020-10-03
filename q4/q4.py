52b519cb9cee40a4e07550a052285d797763dbd2
utkarsh92 <utkarshindolia@cse.iitb.ac.in>
2020-10-03 13:21:15 +0530

    q4 update

--- q4/q4.py
+++ q4/q4.py
@@ -10,15 +10,20 @@ tags = list(map(str.lower, input().split()))
 fname = '_'.join(map(str,tags))
 
 SITE = StackAPI('stackoverflow', key='eV18T0ejHKZNnSWR6YDmdQ((')
-SITE.page_size = 50
-SITE.max_pages = 1
+SITE.page_size = 100
+SITE.max_pages = 5
 ques = SITE.fetch('questions', tagged=';'.join(map(str,tags)), sort='votes')
 #pp = pprint.PrettyPrinter(indent=4)
 #pp.pprint(ques)
+#sys.exit()
 #print('quota_remaining:', ques['quota_remaining'])
 final = []
+count = 0
 for item in ques['items']:
     
+    if count == 50:
+    	break
+
     if 'accepted_answer_id' in item:
         
         qid = item['question_id']
@@ -28,6 +33,7 @@ for item in ques['items']:
         ans = link + "/#" + str(item['accepted_answer_id'])
 
         final.append([qid, utag, link, qtag, ans])
+        count += 1
 
 final = np.array(final)
 
